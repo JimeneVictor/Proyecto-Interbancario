88e1715708d0842ab7f374f9a34ed90e6f053b89
JimeneVictor <162595405+JimeneVictor@users.noreply.github.com>
2024-04-18 19:43:04 -0600

    Final ListaCliente.java

--- ProyectoIntegradorBanca/src/proyectointegradorbanca/ListaCliente.java
+++ ProyectoIntegradorBanca/src/proyectointegradorbanca/ListaCliente.java
@@ -5,6 +5,8 @@ package proyectointregradorbanca;
     private Cliente fin;
 
     public ListaCliente() {
+        this.inicio=null;
+        this.fin=null;
     }
 
     public Cliente getInicio() {
@@ -77,4 +79,50 @@ package proyectointregradorbanca;
     // Si llegamos aquí, significa que no se encontró la cuenta
     System.out.println("La cuenta " + noCuenta + " no existe.");
 }
+     public void agregarCliente(int noCliente, int noCuenta, String direccion, String nombre, String contacto) {
+        Cliente nuevoCliente = new Cliente(noCliente, noCuenta, direccion, nombre, contacto);
+        if (inicio == null) {
+            inicio = nuevoCliente;
+            fin = nuevoCliente;
+        } else {
+            fin.setSiguiente(nuevoCliente);
+            nuevoCliente.setAnteror(fin);
+            fin = nuevoCliente;
+        }
+    }
+    
+    // Método para eliminar un cliente de la lista de gerentes
+    public void eliminarCliente(int noCuenta) {
+        Cliente actual = inicio;
+        while (actual != null) {
+            if (actual.getNoCuenta() == noCuenta) {
+                if (actual == inicio && actual == fin) {
+                    inicio = null;
+                    fin = null;
+                } else if (actual == inicio) {
+                    inicio = actual.getSiguiente();
+                    inicio.setAnteror(null);
+                } else if (actual == fin) {
+                    fin = fin.getAnteror();
+                    fin.setSiguiente(null);
+                } else {
+                    actual.getAnteror().setSiguiente(actual.getSiguiente());
+                    actual.getSiguiente().setAnteror(actual.getAnteror());
+                }
+                System.out.println("Cliente eliminado correctamente.");
+                return;
+            }
+            actual = actual.getSiguiente();
+        }
+        System.out.println("No se encontró ningún cliente con ese número de cuenta.");
+    }
+
+    // Método para mostrar la lista de clientes
+    public void mostrarClientes() {
+        Cliente actual = inicio;
+        while (actual != null) {
+            System.out.println(actual.toString());
+            actual = actual.getSiguiente();
+        }
+    }
 }
